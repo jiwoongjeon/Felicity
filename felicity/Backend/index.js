0f8ae92e53df716815fe12b5d19a0901733fe769
wonleeeee <won8303@gmail.com>
2021-09-21 23:33:21 -0400

    connected express and mysql

---
+++ felicity/Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "wonseok3",
+    database: "felicity"
+});
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected")
+});
+
+app.get("/", (req, res) => {
+    res.send("Main Route");
+})
+
+const port = 3001;
+const server = app.listen(port, () => {
+    console.log(`Server running on Port ${port}`);
+});
+
