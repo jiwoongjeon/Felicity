e936c093151003f427647bdeec7a6a20577ec390
wonleeeee <won8303@gmail.com>
2021-09-25 04:00:05 -0400

    update

--- felicity/Frontend/src/Components/Layout4Cal/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Layout4Cal = styled.div`
-  width: 1000px;
-  height: 500px;
-  display: flex;
-  flex-direction: row;
-  border-radius: 8px;
-`;
