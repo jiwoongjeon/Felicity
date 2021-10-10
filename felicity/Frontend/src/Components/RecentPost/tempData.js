d6569e6c3f6990d0f9ea98c00a080eac2da2d2b0
Justin Lee <wonseuk2125@gmail.com>
2021-10-10 01:22:43 -0400

    의료진 페이지의 Recent Post 컴포넌트

---
+++ felicity/Frontend/src/Components/RecentPost/tempData.js
@@ -0,0 +1,27 @@
+export const POST_DATA = [
+    {
+        no: 162,
+        patient: "Esthera Jackson",
+        symptoms: [
+            {id:"High Fever"}
+            ,
+            {id:"Cough"}
+        ],
+        date: "2021-09-20"
+    }
+    ,
+    {
+        no: 161,
+        patient: "Alexa Liras",
+        symptoms: [
+            {id:"High Fever"}
+        ],
+        date: "2021-09-20"
+    }
+];
+
+export const CATEGORY_DATA = [
+    {id: "Respiratory disease"},
+    {id: "Pediatric care"},
+    {id: "Skin disease"}
+];
