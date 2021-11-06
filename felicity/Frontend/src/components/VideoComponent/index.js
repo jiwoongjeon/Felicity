483c078586548411ad771f0c49a4758bbdc711c2
Justin Lee <wonseuk2125@gmail.com>
2021-11-06 19:07:25 -0400

    Initial commit - DO NOT PULL

---
+++ felicity/Frontend/src/components/VideoComponent/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+const { CameraContainer } = require ("./styles");
+
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+  
+
+export const VideoComponent = () => {
+    return (
+        <>
+        <Webcam
+        audio={false}
+        height={720}
+        ref={React.useRef(null)}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+        />
+        </>
+    );
+}
+export default VideoComponent;
---
+++ felicity/Frontend/src/components/VideoComponent/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CameraContainer = styled.div`
+  height: 412px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px;
+  background: white;
+  border-radius: 8px;
+`;
