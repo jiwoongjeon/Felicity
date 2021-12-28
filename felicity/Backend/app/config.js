9a17347eef43b17f1671a59537c43ff02563ae42
wonleeeee <won8303@gmail.com>
2021-12-27 22:20:15 -0500

    stt & translation, new aws connection

--- felicity/Backend/app/config.js
+++ felicity/Backend/app/config.js
@@ -5,9 +5,9 @@ var config = module.exports;
 
 config.db = mysql.createConnection({
     user: "admin",
-    host: "database-1.cwpvsesnohv2.us-east-2.rds.amazonaws.com",
+    host: "telepdb.cbozhfq3cb5w.us-east-2.rds.amazonaws.com",
     password: `${pw}`,
-    database: "felicitytest"
+    database: "felicity"
 });
 config.db.connect((err) => {
     if (err) throw err;
--- felicity/Backend/app/index.js
+++ felicity/Backend/app/index.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./config")
+const socket = require("socket.io");
+const transcribe = require("../stt");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
+app.use(require("./patientLogin/router"));
 app.use(require("./posts/router"));
 app.use(require("./schedules/router"));
 
+app.get("/audio", (req, res) => {
+    console.log(req.body);
+});
+
 // const port = 3001;
-app.listen(config.express.port, () => {
+const server = app.listen(config.express.port, () => {
     console.log(`Server running on Port ${config.express.port}`);
 });
 
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on("connection", async socket => {
+    socket.on("message-transcribe", async (file) => {
+        const dataURL = file.audio.dataURL.split(",").pop()
+        let fileBuffer = Buffer.from(dataURL, "base64")
+        const result = await transcribe(fileBuffer)
+        console.log(result)
+        // console.log(dataURL)
+        socket.emit("result", result)
+    })
+})
