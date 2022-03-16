b34d4707ecb4eed2ce09bb00333cec2edf177fad
dayeonnam <nam44@purdue.edu>
2022-03-16 11:17:40 +0900

    update changes: subtitle box, record box, patient appointment session

--- felicity/Frontend/src/views/MHT/index3.js
+++ felicity/Frontend/src/views/MHT/index3.js
@@ -3,18 +3,34 @@ import React from "react";
 import LogoImg from '../../Components/assets/Logo.png';
 import { BodyInput, InputBox, Label, Logo, MainContainer, QuestionContainer, SubmitButton, SubTitle, Title } from "./styles";
 
+const Check = () => {
+  const hurt = window.sessionStorage.getItem('hurt');
+  const depart = window.sessionStorage.getItem('depart');
+  const time = window.sessionStorage.getItem('time');
+  return (<div>hurt: {hurt} depart: {depart} time: {time}</div>);
+};
+
 function MHT3() {
+  function sessionStore () { //stores items in sessionStorage
+    var where = document.getElementById('where').value;
+  
+    window.sessionStorage.setItem('where',where);
+  
+    document.location.href = '/MHT4';
+  }
+
   return (
     <MainContainer>
         <QuestionContainer>
             <Logo src={LogoImg}></Logo>
             <Title>Welcome!</Title>
             <SubTitle>Please fill out medical history form below (3/6)</SubTitle>
-            <SubmitButton to={`/MHT4`}>
+            <SubmitButton onClick={sessionStore}>
                 Next</SubmitButton>
             <Label>3. Where does the most severely hurt?  *</Label>
+        
             <InputBox>
-                <BodyInput/>
+                <BodyInput id="where"/>
             </InputBox>
         </QuestionContainer>
     </MainContainer>
