2df63585607df02673da0c7cd341bca4c2103631
Justin Lee <wonseuk2125@gmail.com>
2022-04-05 00:45:21 -0400

    login fix

---
+++ felicity/Frontend/src/views/UserRedirect/login.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+function LoginRedirect({isRole}) {
+  console.log('loginredirect')
+  return (
+    <html lang="en">
+        <body>
+        <div id="root"></div>
+        <Route>
+            { !isRole  && <Redirect to="/Doctor/Home" />}
+            { isRole  && <Redirect to="/Patient/Home" />}
+            <Redirect to="/Patient/Home" />
+        </Route>
+        </body>
+    </html>
+    
+  );
+}
+
+export default LoginRedirect;
