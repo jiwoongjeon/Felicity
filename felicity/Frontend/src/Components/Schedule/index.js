52b657b30174577698d233601f9c332bf16b4b2f
wonleeeee <won8303@gmail.com>
2022-04-30 02:43:45 -0400

    Changed reservation filtering logic: filter from backend

--- felicity/Frontend/src/Components/Schedule/index.js
+++ felicity/Frontend/src/Components/Schedule/index.js
@@ -3,43 +3,40 @@ import React from "react";
 import moment from "moment";
 
 const { ScheduleContainer, Header, AppointmentContainer, AppointmentList, FstColumn, Column, Group, Date, TimeEmail, EditIcon, DeleteIcon, DoctorEmail, ScheduleElement } = require("./styles");
-const today = moment().format("MM-DD-YYYY");
-const counter = 0;
 
-export const Schedule = (props) => {
-    
+export const Schedule = ({ schedule_data }) => {
+
 
     return (
         <ScheduleContainer>
             <Header>My Schedule</Header>
             <AppointmentList>
-            {moment(props.reserved_date).format("MM-DD-YYYY") > today && props.schedule_data.map((data) => (
-                <AppointmentContainer>  
-                    {counter += 1}
-                    <FstColumn to={"/Patient/videocall"}>
-                        <Group>
-                            <Date> {(moment(data.reserved_date).format("MM-DD-YYYY") == today) ? today : data.reserved_date}                            
-                            </Date>
-                            <TimeEmail>{data.reserved_time}</TimeEmail>
-                        </Group>
-                        <Group>
-                            <DeleteIcon>DELETE</DeleteIcon>
-                            <EditIcon>EDIT</EditIcon>
-                        </Group>
-                    </FstColumn>
-                    <Column>
-                        <DoctorEmail>Doctor: {data.doctor_firstName} {data.doctor_lastName}</DoctorEmail>
-                    </Column>
-                </AppointmentContainer>
-                ))}
-                { counter == 0 && <Column>There is no appointment left</Column>}
-               
+                {schedule_data.length ? (schedule_data.map((data) => (
+                    <AppointmentContainer>
+                        <FstColumn to={"/Patient/videocall"}>
+                            <Group>
+                                <Date>{data.reserved_date}</Date>
+                                <TimeEmail>{data.reserved_time}</TimeEmail>
+                            </Group>
+                            <Group>
+                                <DeleteIcon>DELETE</DeleteIcon>
+                                <EditIcon>EDIT</EditIcon>
+                            </Group>
+                        </FstColumn>
+                        <Column>
+                            <DoctorEmail>Doctor: {data.firstname} {data.lastname}</DoctorEmail>
+                        </Column>
+                    </AppointmentContainer>
+                ))) :
+                    (
+                        <Column>There is no appointment left</Column>
+                    )}
 
-        
-        </AppointmentList>
+
+            </AppointmentList>
         </ScheduleContainer>
     );
-            }
+}
 
 
 
