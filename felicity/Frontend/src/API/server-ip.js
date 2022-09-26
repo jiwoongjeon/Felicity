33dd8c1f29d74fa74d6c3721f517b10a34b22334
Justin Lee <wonseuk2125@gmail.com>
2022-09-25 23:10:56 -0400

    ddddd

--- felicity/Frontend/src/API/server-ip.js
+++ felicity/Frontend/src/API/server-ip.js
@@ -2,6 +2,6 @@
 // const API_URL = "https://telepossibleserver.com";
 
 // Use this for local
-const API_URL = "http://localhost:3001";
+// const API_URL = "http://localhost:3001";
 
 export default API_URL;
