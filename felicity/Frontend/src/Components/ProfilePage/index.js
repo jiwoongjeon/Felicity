f348fd689f61ae11e8ee4380cc23e39e50dd17ff
Justin Lee <wonseuk2125@gmail.com>
2022-10-16 23:34:36 -0400

    1016 - Juhwan Conflict resolve

--- felicity/Frontend/src/Components/ProfilePage/index.js
+++ felicity/Frontend/src/Components/ProfilePage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { TextField } from '@mui/material';
-import { TimeButton, Content, Divider, InfoContainer, Label, PhotoArea, PictureContainer, ProfileContainer, Row, DeleteButton } from "./styles";
+import { TimeButton, Content, Divider, InfoContainer, Button, Label, PhotoArea, PictureContainer, ProfileContainer, Row, DeleteButton, UserName } from "./styles";
 
 export const ProfilePage = (props) => {
 
@@ -26,55 +26,20 @@ export const ProfilePage = (props) => {
     
     return(
         <ProfileContainer>
+            <Button>Edit</Button>
             <PictureContainer>
                 <PhotoArea img={fileImage}/>
-                <input type='file' className='imgInput' id='logoImg' accept='image/*' name='file' onChange={saveFileImage}/>
-                {fileImage && <DeleteButton onClick={deleteFileImage}>Delete image</DeleteButton>}
+                {/* <input type='file' className='imgInput' id='logoImg' accept='image/*' name='file' onChange={saveFileImage}/>
+                {fileImage && <DeleteButton onClick={deleteFileImage}>Delete image</DeleteButton>} */}
             </PictureContainer>
             <Divider/>
+            <UserName>Name</UserName>
             <InfoContainer>
-                <Label>NAME</Label>
-                <Content>{props.name}</Content>
-                <Label>Date of birth</Label>
-                <Label>Sex</Label>
-                <Label>EMAIL</Label>
-                <Content>{props.email}</Content>
-                {props.isDoctor && <Label>Profession</Label>}
-            
-                {props.isDoctor && <Label>Education</Label>}
-                {props.isDoctor && <Label>Phone</Label>}
-                
-                
-                {props.isDoctor && <Label>AVAILABLE TIME</Label>}aaaa
-                {props.isDoctor && <Content>{props.time}</Content>}
-                {!props.isDoctor && <Label>Underlying Disease</Label>}
-                {!props.isDoctor && <Label>Past medical record</Label>}
-
-
-                {isTime &&
-                    <Row>
-                        <TextField
-                            value={start_time}
-                            id="time"
-                            label="Available from..."
-                            type="time"
-                            sx={{ width: 200, marginRight: 3, marginBottom:3 }}
-                            InputLabelProps={{ shrink: true }}
-                            inputProps={{ step: 300 }}
-                            onChange={(event) => {setStartTime(event.target.value); console.log(event.target.value)}}/>
-                        <TextField
-                            value={end_time}
-                            id="time"
-                            label="Available by..."
-                            type="time"
-                            sx={{ width: 200, marginRight: 3, marginBottom:3 }}
-                            InputLabelProps={{ shrink: true }}
-                            inputProps={{ step: 300 }}
-                            onChange={(event) => {setEndTime(event.target.value)}}/>
-                    </Row>}
-                {props.isDoctor && !isTime && <TimeButton onClick={(e) => {changeTime(!isTime)}}>Change time</TimeButton>}
-                {props.isDoctor && isTime && start_time && end_time && <TimeButton onClick={setTime}>Confirm</TimeButton>}
-                {props.isDoctor && isTime && (!start_time || !end_time) && <TimeButton onClick={(e) => {changeTime(!isTime)}}>Cancel</TimeButton>}
+            <Label>E-mail:</Label>
+                <Divider></Divider>
+                <Label>D.O.B:</Label>
+                <Divider></Divider>
+                <Label>Sex:</Label>
             </InfoContainer>
 
         </ProfileContainer>
