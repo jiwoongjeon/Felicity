574c6a9d5e4987a07bb7220684e9ec8680eb855f
akdrh3 <im7738@naver.com>
2023-03-05 22:22:44 -0500

    profile update API: Underlying disease, preferred lang, profession, education, available time

--- felicity/Backend/app/profile/router.js
+++ felicity/Backend/app/profile/router.js
@@ -64,6 +64,92 @@ function postProfileImagePath(req, res) {
     }
 }
 
+function updateUnderlyingDisease(req, res){
+    const patientId = req.body.patientId;
+    const underDisease = req.body.underlyingDisease;
+
+    profile.updateUnderlyingDisease(patientId, underDisease, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating underlting disease"});
+        }
+        else {res.json(result);}
+    })
+
+}
+
+function updatePatientPreferredLang(req, res){
+    const patientId = req.body.patientId;
+    const lang = req.body.preferredLang;
+
+    profile.updatePatientPreferredLang(patientId, lang, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating preferred language"});
+        }
+        else {res.json(result);}
+    })
+}
+
+function updateDoctorPreferredLang(req, res){
+    const doctorId = req.body.doctorId;
+    const lang = req.body.preferredLang;
+
+    profile.updateDoctorPreferredLang(doctorId, lang, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating preferred language"});
+        }
+        else {res.json(result);}
+    })
+}
+
+function updateProfession(req, res){
+    const doctorId = req.body.doctorId;
+    const profession = req.body.prefession;
+
+    profile.updateProfession(doctorId, profession, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating profession"});
+        }
+        else {res.json(result);}
+    })
+}
+
+function updateEducation(req, res){
+    const doctorId = req.body.doctorId;
+    const education = req.body.education;
+
+    profile.updateEducation(doctorId, education, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating education"});
+        }
+        else {res.json(result);}
+    })
+}
+function updateAvailableTime(req, res){
+    const doctorId = req.body.doctorId;
+    const timeA = req.body.timeA;
+    const timeB = req.body.timeB;
+
+    profile.updateAvailableTime(doctorId, timeA, timeB, (err, result) => {
+        if (err){
+            console.log(err);
+            res.json({ errMsg: "Error: Failed on updating available time"});
+        }
+        else {res.json(result);}
+    })
+}
+
+router.post("/updateUnderlyingDisease", updateUnderlyingDisease);
+router.post("/updatePatientPreferredLang", updatePatientPreferredLang);
+router.post("/updateDoctorPreferredLang", updateDoctorPreferredLang);
+router.post("/updateProfession", updateProfession);
+router.post("/updateEducation", updateEducation);
+router.post("/updateAvailableTime", updateAvailableTime);
+
 router.post("/doctor_conv", getProfileImagePath);
 router.post("/post_profile_image_path", postProfileImagePath);
 router.post("/post_profile_image", (req, res) => {
@@ -107,5 +193,7 @@ router.post("/remove_profile_image", (req, res) => {
         console.log("Delete file successfully")
     })
 })
+
+
 
 module.exports = router;
